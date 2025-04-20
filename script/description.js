eb7016860a4a4ba09c33ae0a42d4af6ea1594c36
NagiFish <94735845+nezikog@users.noreply.github.com>
2025-04-20 22:18:14 +0300

    \description.js

--- script/description.js
+++ script/description.js
@@ -1,21 +1,3 @@
-
-
-
-
-
-
-
-
-
-#Сделать вместо const create обычную функцию
-
-
-
-
-
-
-
-
 const mn = document.querySelector("main");
 
 const avatar = document.querySelector(".avatar");
@@ -37,76 +19,51 @@ const results = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1").then(
 console.log(results);
 
 
+function interactive(){
+  let div = document.createElement("div")
 
-const create = await Promise.all(
-  results.results.map(async (result) => {
-    setTimeout(() => {}, 5000);
-    const pokeData = await fetch(result.url).then((pokeData) =>
-      pokeData.json()
-    );
+  //Сделать способности через forEach
+  //В kr_info занести инфо об ударах и тд. более второстепенную информацию занести в bio
+  div.innerHTML = `<div class="info">
 
-    // let image = document.createElement("div");
-    // image.innerHTML = `<img class="avatar_img" src=${
-    //                       pokeData?.sprites?.other?.dream_world
-    //                         ?.front_default ?? pokeData?.sprites?.front_default
-    //                     } class="avatar_img">`
-
-    // let nameen = document.createElement("div");
-    // nameen.innerHTML = `<h3 class="name_h3">${result.name}</h3>`
-    // namer.append(nameen)
-
-    // avatar.append(image);
-
-    let div = document.createElement("div")
-    
-    let id = "0";
-
-    let mm = data.moves;
-
-    console.log(mm)
-
-    //Сделать способности через forEach
-    //В kr_info занести инфо об ударах и тд. более второстепенную информацию занести в bio
-    div.innerHTML = `<div class="info">
-
-        <div class="avatar">
-          <img class="avatar_img" src=${
-            data?.sprites?.other?.dream_world?.front_default ?? pokeData?.sprites?.front_default
-          }
-          </div>
+      <div class="avatar">
+        <img class="avatar_img" src=${
+          data?.sprites?.other?.dream_world?.front_default ?? pokeData?.sprites?.front_default
+        }
         </div>
+      </div>
 
-        <div class="name">
-          <h1 class="name_h3">
-            ${data.name}
-          </h1>
-        </div>
+      <div class="name">
+        <h1 class="name_h3">
+          ${data.name}
+        </h1>
+      </div>
 
-        <div class="kr_info">
-          <h2>
-          ${data.abilities["0"].ability.name}
-          </h2>
-          <h2>
-          ${data.abilities["1"].ability.name}
-          </h2>
-          <h2>
-          ${data.base_experience}
-          </h2>
-          <h2>
-          ${data.moves["0"].move.name}
-          </h2>
-        </div>
+      <div class="kr_info">
+        <h2>
+        ${data.abilities["0"].ability.name}
+        </h2>
+        <h2>
+        ${data.abilities["1"].ability.name}
+        </h2>
+        <h2>
+        ${data.base_experience}
+        </h2>
+        <h2>
+        ${data.moves["0"].move.name}
+        </h2>
+      </div>
 
-        <div class="bio">
-          <p>${data.weight}</p>
-          <p>${data.height}</p>
-        </div>
-        `
-    
-      mn.append(div)
-  })
-);
+      <div class="bio">
+        <p>${data.weight}</p>
+        <p>${data.height}</p>
+      </div>
+      `
+  
+    mn.append(div)
+}
 
+interactive();
 
 console.log(data);
 
